24d7bffaaf4a678e8487fedf950e6b31d70224b2
fjl <18618166564@163.com>
2022-03-10 17:38:07 +0800

    add

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,11 +1,16 @@
+import { isArray, isObject } from "../shared";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
   patch(vnode, container);
 }
 function patch(vnode: any, container: any) {
-  // 组件
-  processComponent(vnode, container);
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    // 组件
+    processComponent(vnode, container);
+  }
 }
 
 function processComponent(vnode: any, container: any) {
@@ -23,4 +28,23 @@ function setupRenderEffect(instance: any, container: any) {
   // 执行 render 是为了获取 vnode，然后进行 patch
   const subTree = instance.render();
   patch(subTree, container);
+}
+function processElement(vnode: any, container: any) {
+  const el = document.createElement(vnode.type);
+  const { props, children } = vnode;
+
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (isArray(children)) {
+    for (const child of children) {
+      patch(child, el);
+    }
+  }
+
+  for (const prop in props) {
+    let val = props[prop];
+    el.setAttribute(prop, val);
+  }
+
+  container.append(el);
 }
